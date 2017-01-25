bbbc4e419df1c42ba48e14b50b06cc263f7e3879
Wian Vos <wian.vos@cinqict.nl>
2017-01-25 11:02:09 +0100

    Makefile) Terraform Apply, syncing terraform.tfstate

--- terraform/environments/deathstar/scripts/terraform_install.sh
+++ terraform/environments/deathstar/scripts/terraform_install.sh
@@ -20,11 +20,10 @@ unzip terraform.zip > /dev/null
 chmod +x terraform
 sudo mkdir /opt/terraform
 sudo chown ubuntu /opt/terraform
-sudo chmod 0664 /opt/terraform
+sudo chmod 0770 /opt/terraform
 sudo mv terraform /opt/terraform
 
 echo "initializing the consul state backend"
-echo $ENV
 cd /opt/terraform
 ./terraform remote config \
     -backend=consul \
